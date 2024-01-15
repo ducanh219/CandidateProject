61889516979aa4396328c07a87e420cf0971944d
ducanh219 <155826567+ducanh219@users.noreply.github.com>
2024-01-15 11:00:22 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,7 @@
+
+public class main {
+    public static void main(String[] args) {
+        CandidateController candidateController = new CandidateController();
+        candidateController.run();
+    }
+}
